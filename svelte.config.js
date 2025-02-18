ef54c6dfe2fe8647a616d608bfb6728201db64da
wolfndavid <cdw8481@nyu.edu>
2025-02-18 17:57:07 -0500

    added gh-apges

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,18 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.env.NODE_ENV === 'development';
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
-}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : '/snow_wolf', // Replace YOUR_REPO_NAME with your actual GitHub repo name
+    },
+    appDir: 'app',
+    prerender: {
+      default: true,
+    },
+  },
+};
